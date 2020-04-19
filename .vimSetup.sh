46c6032178a742c8539d5a5a21b1fbc58506a772
waltonpcoutinho <walton.coutinho@gmail.com>
2020-04-18 22:03:39 -0300

    Added script .vimSetup

---
+++ .vimSetup.sh
@@ -0,0 +1,23 @@
+#clone Vundle from github
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+#install nerdcommenter
+curl -fLo ~/.vim/plugin/NERD_Commenter.vim --create-dirs \
+  https://raw.githubusercontent.com/preservim/nerdcommenter/master/plugin/NERD_commenter.vim
+curl -fLo ~/.vim/doc/NERD_Commenter.txt --create-dirs \
+  https://raw.githubusercontent.com/preservim/nerdcommenter/master/doc/NERD_commenter.txt
+
+#install ack search tool
+sudo apt-get install ack
+
+#install ctags neede by gutentags
+sudo apt-get install ctags
+
+#To install plugins from command line
+vim +BundleInstall +qall
+
+
+cp .vim/bundle/vim-afterglow/autoload/airline/themes/afterglow.vim .vim/colors/
+
+
+
