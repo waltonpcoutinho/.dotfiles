14d430a422278e877dbb429090f2cbb29c775f34
waltonpcoutinho <walton.coutinho@gmail.com>
2023-04-07 00:53:55 -0300

    attempt to fix the creation of symb links to always create into home folder

--- .create_symlinks.sh
+++ .create_symlinks.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-ln -sf .dotfiles/.bashrc .bashrc
-ln -sf .dotfiles/.vimrc .vimrc
-ln -sf .dotfiles/.tmux.conf .tmux.conf
+ln -sf .dotfiles/.bashrc ~/.bashrc
+ln -sf .dotfiles/.vimrc ~/.vimrc
+ln -sf .dotfiles/.tmux.conf ~/.tmux.conf
